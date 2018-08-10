4a80f5afe28969fad7f0f36dd836d5e7d7fe7349
linuxsjun <linuxsjun@gmail.com>
2018-08-10 15:32:40 +0800

    change data to pureftp user db

--- web/models.py
+++ web/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-    name=models.CharField(max_length=32)
-    crnu=models.IntegerField(default=1)
+    status = models.BooleanField(default=1)
+    user=models.CharField(max_length=32)
+    password=models.CharField(max_length=64)
+    uid=models.CharField(max_length=11)
+    gid=models.CharField(max_length=11)
+    dir=models.CharField(max_length=128)
+    ulbandwidth=models.IntegerField(max_length=5)
+    dlbandwidth=models.IntegerField(max_length=5)
+    comment=models.CharField(max_length=128)
+    ipaccess=models.CharField(max_length=15)
+    quotasize=models.IntegerField(max_length=5)
+    quotaFiles=models.IntegerField(max_length=11)
+    createdate=models.DateField(auto_now_add=True)
+    lastedate=models.DateField()
+
