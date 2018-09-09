f8a510807adc9cad234274172a62b90b27923799
linuxsjun <linuxsjun@gmail.com>
2018-09-10 00:15:34 +0800

    update view bootstrap

--- static/js/property_form.js
+++ static/js/property_form.js
@@ -2,15 +2,10 @@ $(document).ready(function () {
     $('.sheet>div>div').css("display","none")
     $($('.sheet>div>div')[0]).css("display","block")
     $('.sheet>ul>li>a').click(function () {
-        // $(this).css("background","#dddddd")
-        // $(this).css("border","0px solid black")
-        //
-        // // alert($(this).index())
-        // $(this).css("background","white")
-        // $(this).css("border","1px solid black")
-        // $(this).css("border-bottom","1px solid white")
-        // $(this).css("border-Top","2px solid #cccc00")
+        $('.sheet>ul>li>a').css("background","#dddddd")
 
+        // // alert($(this).index())
+        $(this).css("background","white")
 
         $('.sheetcontext>div').css("display","none")
         $('.sheetcontext>div')[$(this).parent().index()].style.display='block'
